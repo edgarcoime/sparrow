1bd8fd057647dfd7ed05648292c1f0a07cfb14ec
Edgar Coime <ecoime.dev@gmail.com>
2026-02-26 13:01:42 -0800

    feat: remove node adapter and update mise configs

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -9,7 +8,6 @@ import wasm from 'vite-plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
 
   vite: {
